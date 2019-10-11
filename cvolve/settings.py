95e1b2854283872ea147f9373e030b343d4484be
Damian Rubio <damian@zapiens.org>
2019-10-11 21:30:29 +0200

    Added Django Templating base

--- cvolve/settings.py
+++ cvolve/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'cvolve.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'cvolve/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
