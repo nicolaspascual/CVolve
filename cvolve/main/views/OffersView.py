7f486a741d78a7a66b2abda9078e35bc4e4a14c8
Nicolás Pascual González <nicolaspascualgonzalez@gmail.com>
2019-10-12 17:33:54 +0200

    Added ordering and distance

--- cvolve/main/views/OffersView.py
+++ cvolve/main/views/OffersView.py
@@ -7,7 +7,8 @@ from cvolve.main import models
 class OffersView(View):
     
     def get(self, request):
-        offers = models.JobOffer.objects.all()
+        user = models.User.objects.get(pk=request.user.id)
+        offers = user.get_offers_sorted_by_distance(return_distances=True)
 
         return render(request, 'offers.html', {
             'offers': offers
