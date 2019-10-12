0f8edfe7a86adafb84db8c3c7d3250d6e805c778
Nicolás Pascual González <nicolaspascualgonzalez@gmail.com>
2019-10-12 10:47:01 +0200

    Finished loaders

--- cvolve/main/models/user_experience.py
+++ cvolve/main/models/user_experience.py
@@ -6,7 +6,7 @@ class UserExperience(models.Model):
     role = models.CharField('Role', max_length=150)
     company = models.CharField('Company', max_length=150)
     start_date = models.DateField('Start Date')
-    end_date = models.DateField('End Date', blank=True)
+    end_date = models.DateField('End Date', blank=True, null=True)
     summary = models.TextField('Summary')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
